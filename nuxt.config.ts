e25b3d592cae3b8cd7fb07a628defa6096ef3c67
Pablo Rosa <pablo.rosa@altruist.com>
2024-10-07 11:17:58 -0400

    implementing SSG to all routes under blogs/*

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,70 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import { createClient } from "@sanity/client";
 
-export default defineNuxtConfig({
-    devtools: {
-        enabled: true,
-
-        timeline: {
-            enabled: true,
-        },
-    },
-    nitro: {
-        prerender: {
-            crawlLinks: true,
-            routes: ["/"],
-        },
-    },
-    routeRules: {
-        // prerender index route by default
-        "/": { prerender: true },
-    },
-    build: {
-        transpile: ["vuetify"],
-    },
-    modules: [
-        "@nuxtjs/sanity",
-        (_options, nuxt) => {
-            nuxt.hooks.hook("vite:extendConfig", (config) => {
-                // @ts-expect-error
-                config.plugins.push(vuetify({ autoImport: true }));
-            });
-        },
-    ],
-    sanity: {
+// Function to fetch slugs from Sanity
+async function fetchSlugs() {
+    const sanityClient = createClient({
         projectId: "ombs0vlk",
-        dataset: "vueproject",
-        apiVersion: "2022-03-25",
-    },
-    vite: {
-        vue: {
-            template: {
-                transformAssetUrls,
+        dataset: "vueproject", // Replace with your Sanity dataset
+        useCdn: true,
+    });
+
+    const posts = await sanityClient.fetch('*[_type == "post"]{slug}');
+    return posts.map((post) => post.slug.current);
+}
+
+export default defineNuxtConfig(async () => {
+    const slugs = await fetchSlugs();
+    const dynamicRoutes = slugs.map((slug) => `/blog/${slug}`);
+
+    return {
+        target: "static",
+        devtools: {
+            enabled: true,
+            timeline: {
+                enabled: true,
             },
         },
-    },
+        nitro: {
+            prerender: {
+                crawlLinks: true,
+                routes: ["/", ...dynamicRoutes],
+            },
+        },
+        routeRules: {
+            "/": { prerender: true },
+        },
+        build: {
+            transpile: ["vuetify"],
+        },
+        modules: [
+            "@nuxtjs/sanity",
+            (_options, nuxt) => {
+                nuxt.hooks.hook("vite:extendConfig", (config) => {
+                    config.plugins.push(vuetify({ autoImport: true }));
+                });
+            },
+        ],
+        vite: {
+            plugins: [
+                vuetify({
+                    autoImport: true,
+                    styles: { configFile: "path/to/your/vuetify/styles.scss" },
+                }),
+            ],
+        },
+        sanity: {
+            projectId: "ombs0vlk",
+            dataset: "vueproject",
+            apiVersion: "2022-03-25",
+        },
+        vite: {
+            vue: {
+                template: {
+                    transformAssetUrls,
+                },
+            },
+        },
+    };
 });
